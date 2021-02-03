bfb223913cb27b67eeeae155b3d40a24a28bcdc6
Randi Schreiner <mimirandi21@gmail.com>
2021-02-03 17:10:56 -0600

    Done.

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class CatComponent extends Component {
-  render() {
-    return (
-      <div className="bar" id="cat">
-        <img src="/cat.gif" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="bar" id="cat">
+				<img alt="cat" src="/cat.gif" />
+			</div>
+		);
+	}
 }
--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export class GraceHopperQuoteComponent extends Component {
+export default class GraceHopperQuoteComponent extends Component {
 	render() {
 		return (
 			<div className="bar" id="hopper">
 				<i>
 					<p>
-						&quot;The most dangerous phrase in the language is, ‘We’ve always done it this way.’&quot;
+						&quot;The most dangerous phrase in the language is, ‘We’ve always
+						done it this way.’&quot;
 					</p>
 				</i>
 				<br />
